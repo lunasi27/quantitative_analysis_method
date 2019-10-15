db33f6901062cd9fe44cef74fd964844564b3486
lunasi27 <wanghao27@163.com>
2019-10-15 23:02:01 +0800

    make web workable

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from app.views import init_blueprint
 from app.config import config
-from app.services import db
+# from app.services import db
 
 
 def create_app():
@@ -9,5 +9,5 @@ def create_app():
     app.config.from_object(config.get('default'))
     app.debug = True
     init_blueprint(app)
-    db.init_app(app)
+    # db.init_app(app)
     return app
