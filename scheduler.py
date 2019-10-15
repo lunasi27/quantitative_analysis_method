01127e464bd8a13f4a5dae12b4d80832dcd31326
lunasi27 <wanghao27@163.com>
2019-10-15 16:26:57 +0800

    create new schedular

--- scheduler.py
+++ scheduler.py
@@ -14,17 +14,20 @@ def update():
 
 
 def exec_select():
-    cmd = []
-    subprocess.call()
-    print()
+    update()
+    # rqalpha run -f main.py -s 2019-10-14
+    cmd = ['rqalpha', 'run', '-f', 'main.py', '-s', '%s' % datetime.now().date()]
+    subprocess.call(cmd)
+    print('Calculate new selected stocks.')
 
 
 if __name__ == '__main__':
-    update()
-    # scheduler = BlockingScheduler()
-    # scheduler.add_job(tick, trigger='cron', hour=15, minute=31)
-    # print('Press Ctrl+C to exit')
-    # try:
-    #     scheduler.start()
-    # except (KeyboardInterrupt, SystemExit):
-    #     pass
+    # update()
+    # exec_select()
+    scheduler = BlockingScheduler()
+    scheduler.add_job(exec_select, trigger='cron', hour=21)
+    print('Press Ctrl+C to exit')
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
